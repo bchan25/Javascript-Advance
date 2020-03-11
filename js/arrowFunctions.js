90e641b34b9b94f3ebe0a539744f666cc6e67fed
Ben Chan <ben@chanhq.com>
2020-03-11 14:37:42 +0000

    Arrow functions notes added

---
+++ js/arrowFunctions.js
@@ -0,0 +1,17 @@
+
+// Function
+/* 
+function add (a, b){
+    return a + b;
+}
+
+*/
+
+// Arrow Functions
+// Single Return
+const add = (a, b) => a + b;
+
+// Alternative
+const multiple = (a, b) => {
+    return a * b;
+}
