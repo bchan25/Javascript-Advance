68ddfe79bd796921d540af60fff0db41680c247b
Ben Chan <ben@chanhq.com>
2020-03-11 13:52:41 +0000

    Added ternary operator example

---
+++ js/controlflow.js
@@ -0,0 +1,11 @@
+// Control Flow
+
+// Ternary Operator
+// Condition ? true : false;
+
+function isUserValid(bool){
+    return bool
+}
+
+var answer = isUserValid(true) ? "You may enter" : "Access Denied";
+
